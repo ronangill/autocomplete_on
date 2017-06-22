814370118039d7b15ff733ad83453b92aaa653ff
ronangill <ronan@jajoro.com>
2017-06-22 11:48:45 +0100

    changed to use querySelectorAll and forEach

--- autocomplete.js
+++ autocomplete.js
@@ -1,22 +1,8 @@
-// pathc for the original project autocomplete_on - author kzahel
-// I've rewrite the code which is supposed to work with every webpage
-
 function enableAutocomplete() {
-'use strict';
-var ac,c,f,fa,fe,fea,x,y,z;
-ac="autocomplete"; c=0;    f=document.forms;
-for(x=0;x<f.length;x++)    {
-    fa=f[x].attributes;
-    for(y=0;y<fa.length;y++) {
-        if(fa[y].name.toLowerCase()==ac) { fa[y].value="on";c++; } }
-        fe=f[x].elements;
-    for(y=0;y<fe.length;y++) {
-        fea=fe[y].attributes;
-        for(z=0;z<fea.length;z++) {
-            if(fea[z].name.toLowerCase()==ac) { fea[z].value="on";c++; }
-            }
-        }
-    }
+  'use strict';
+  document.querySelectorAll('[autocomplete]').forEach(function(element){
+    element"autocomplete"] = "on";
+  });
 }
 
 // The password manager code checks for "autocomplete=off" in a callback
